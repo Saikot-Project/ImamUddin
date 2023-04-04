13df1d50c34735222bba6e961e3379127ba6e61d
Saikot-Project <imam.saikot@gmail.com>
2023-04-03 21:50:39 -0400

    Project create by QA Lead Saikot

---
+++ src/test/java/saikot_basic_practice/Saikot_Uddin_Imam.java
@@ -0,0 +1,25 @@
+package saikot_basic_practice;
+
+public class Saikot_Uddin_Imam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int intialCash = 100;
+		int wid = 75;
+		int deposite = 500;
+		int total = intialCash-wid+deposite;
+		System.out.println("total = "+ total);
+		String name = "Md Imam Uddin Saikot";
+				System.out.println(name);
+				
+	 double Cash = 259.99;
+	 double Pound = 69.85;
+	 double Taka = 999.99;
+	 double Total = Cash+Pound+Taka;
+	 System.out.println("Main Blance" + Total);
+		
+
+	}
+
+}
